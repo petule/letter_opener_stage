ba4ba524de6a65b9f10524abf45737121902ffb5
petra markova <petra@pkis.cz>
2019-02-07 13:10:30 +0100

    add count Letters

--- app/helpers/letters_helper.rb
+++ app/helpers/letters_helper.rb
@@ -2,7 +2,7 @@ module LettersHelper
   def inbox_link
     content_tag(:div, class: "letter-link", style: 'position: absolute; top: 0px; right: 0px;') do
       content_tag(:a,  href: letter_opener_letters_path, title: 'Inbox') do
-        image_tag('letter_opener_stage/letter.svg', class: 'letter-icon', style: 'width: 25px;')
+        image_tag('letter_opener_stage/letter.svg', class: 'letter-icon', style: 'width: 25px;') + "(#{Letter.all.count})"
       end
 
     end
