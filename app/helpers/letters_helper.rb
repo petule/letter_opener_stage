c802112fbe32b82f83ee6666f8dcfec5d63b6c82
petra markova <petra@pkis.cz>
2019-05-15 23:10:41 +0200

    fix right position

--- app/helpers/letters_helper.rb
+++ app/helpers/letters_helper.rb
@@ -1,6 +1,6 @@
 module LettersHelper
   def inbox_link
-    content_tag(:div, class: "letter-link", style: 'position: absolute; top: 0px; right: 0px;') do
+    content_tag(:div, class: "letter-link", style: 'position: absolute; top: 0px; right: 250px;') do
       content_tag(:a,  href: letter_opener_letters_path, title: 'Inbox', style: 'text-decoration: none') do
         image_tag('letter_opener_stage/letter.svg', class: 'letter-icon', style: 'width: 25px;') + "<strong>(#{Letter.all.count})</strong>".html_safe
       end
